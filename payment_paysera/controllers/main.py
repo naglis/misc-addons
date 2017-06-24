6a37a6873331e334847e1351c6703701c7b0286c
Naglis Jonaitis <naglis@mailbox.org>
2017-06-24 12:27:49 +0300

    payment_paysera: specifies supported HTTP methods in controller endpoints

--- payment_paysera/controllers/main.py
+++ payment_paysera/controllers/main.py
@@ -13,25 +13,25 @@ class PayseraController(http.Controller):
 
     @http.route([
         '/payment/paysera/accept',
-    ], type='http', auth='none', csrf=False)
-    def paysera_payment_accept(self, **post_data):
+    ], type='http', auth='none', methods=['GET'])
+    def paysera_payment_accept(self, **params):
         # We receive payment info when the user is redirected after a
         # successfull payment, too, although the status will most likely be `2`
         # (pending) at this point.
         request.env['payment.transaction'].sudo().form_feedback(
-            post_data, 'paysera')
+            params, 'paysera')
 
         return werkzeug.utils.redirect('/shop/payment/validate')
 
     @http.route([
         '/payment/paysera/cancel',
-    ], type='http', auth='none', csrf=False)
-    def paysera_payment_cancel(self, **post_data):
-        return werkzeug.utils.redirect('/shop')
+    ], type='http', auth='none', methods=['GET'])
+    def paysera_payment_cancel(self, **params):
+        return werkzeug.utils.redirect('/shop/payment')
 
     @http.route([
         '/payment/paysera/callback',
-    ], type='http', auth='none', csrf=False)
+    ], type='http', auth='none', methods=['GET', 'POST'], csrf=False)
     def paysera_payment_callback(self, **post_data):
         request.env['payment.transaction'].sudo().form_feedback(
             post_data, 'paysera')
