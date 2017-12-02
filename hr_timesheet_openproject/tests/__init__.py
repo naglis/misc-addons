1ceb1f2897e9ff0e3610aab370aeb9d9dbddb9a0
Naglis Jonaitis <naglis@mailbox.org>
2017-12-02 20:33:56 +0200

    removes hr_timesheet_openproject addon

--- hr_timesheet_openproject/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Naglis Jonaitis
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import (
-    test_timesheet_import,
-    test_utils,
-)
