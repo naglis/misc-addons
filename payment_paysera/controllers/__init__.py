91d0c499a7d4bea17c29d8c4e85f5403e9c5b2fe
Naglis Jonaitis <naglis@mailbox.org>
2018-01-13 02:24:45 +0200

    adds copyright headers

--- payment_paysera/controllers/__init__.py
+++ payment_paysera/controllers/__init__.py
@@ -1,3 +1,5 @@
 # -*- coding: utf-8 -*-
+# Copyright 2018 Naglis Jonaitis
+# License AGPL-3 or later (https://www.gnu.org/licenses/agpl).
 
 from . import main
--- payment_paysera/tests/test_paysera.py
+++ payment_paysera/tests/test_paysera.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# Copyright 2018 Naglis Jonaitis
+# License AGPL-3 or later (https://www.gnu.org/licenses/agpl).
 
 import unittest
 import xml.etree.ElementTree as ET
