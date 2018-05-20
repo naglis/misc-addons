7f6e0209dd8fe69b0fc5751f703e7b338db5976b
Naglis Jonaitis <naglis@mailbox.org>
2018-05-20 21:09:22 +0300

    adds sec_defused_xmlrpc module

---
+++ sec_defused_xmlrpc/README.rst
@@ -0,0 +1,9 @@
+==============
+Defuse XML-RPC
+==============
+
+This module mitigates several XML-related attacks which are possible via Odoo's
+XML-RPC, most notably:
+
+- Billion Laughs (also known as the exponential entity expansion) attack.
+- gzip decompression bombs.
---
+++ sec_defused_xmlrpc/tests/test_defused_xmlrpc.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Naglis Jonaitis
+# License AGPL-3 or later (https://www.gnu.org/licenses/agpl).
+
+import unittest
+import xmlrpclib
+
+import defusedxml.common
+
+from .. import (
+    install_defusedxml,
+    uninstall_defusedxml,
+)
+
+XML_BOMB_PAYLOAD = '''<?xml version="1.0"?>
+<!DOCTYPE lolz [
+ <!ENTITY lol "lol">
+ <!ELEMENT lolz (#PCDATA)>
+ <!ENTITY lol1 "&lol;&lol;&lol;&lol;&lol;&lol;&lol;&lol;&lol;&lol;">
+ <!ENTITY lol2 "&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;">
+ <!ENTITY lol3 "&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;">
+ <!ENTITY lol4 "&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;">
+ <!ENTITY lol5 "&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;">
+ <!ENTITY lol6 "&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;">
+ <!ENTITY lol7 "&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;">
+ <!ENTITY lol8 "&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;">
+ <!ENTITY lol9 "&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;">
+]>
+<lolz>&lol9;</lolz>
+'''
+
+
+class TestInstallDefusedXML(unittest.TestCase):
+
+    def setUp(self):
+        super(TestInstallDefusedXML, self).setUp()
+        uninstall_defusedxml()
+
+    def tearDown(self):
+        super(TestInstallDefusedXML, self).tearDown()
+        uninstall_defusedxml()
+
+    def test_xmlrpc_lib_patched_xml_bomb_raises_entities_forbidden_error(self):
+        install_defusedxml()
+
+        with self.assertRaises(defusedxml.common.EntitiesForbidden):
+            xmlrpclib.loads(XML_BOMB_PAYLOAD)
