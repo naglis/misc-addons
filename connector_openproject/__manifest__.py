0ff8159ceb26eeb475074735d3eb293dafc06809
Naglis Jonaitis <naglis@mailbox.org>
2018-08-21 22:58:17 +0300

    connector_openproject: extracts test backend record to demo data
    Closes #9.

--- connector_openproject/__manifest__.py
+++ connector_openproject/__manifest__.py
@@ -31,6 +31,9 @@
         'views/openproject_project_task.xml',
         'views/openproject_project_project.xml',
     ],
+    'demo': [
+        'demo/openproject.xml',
+    ],
     'images': [
         'static/description/main_screenshot.png',
     ],
